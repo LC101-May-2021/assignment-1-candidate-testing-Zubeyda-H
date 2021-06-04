71d5936b81045db489ae45568ceeef317fc1c9c5
zubeyda <>
2021-06-04 06:22:05 +0000

    update string

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,13 +12,13 @@ let candidateAnswer="Your Answer";
 
 let numberofCorrectAnswers = 0;
 let questions = [
-  '1) Who was the first American woman in space?',
-  '2) True or false: 5000 meters = 5 kilometers.',
-  '3) (5 + 3)/2 * 10 = ?',
-  '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?',
-  '5) What is the minimum crew size for the International Space Station (ISS)?'
+  'Who was the first American woman in space?',
+  'True or false: 5000 meters = 5 kilometers.',
+  '(5 + 3)/2 * 10 = ?',
+  'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?',
+  'What is the minimum crew size for the International Space Station (ISS)?'
 ];
-let correctAnswers = ['sally ride','true', '40', 'trajectory', '3'];
+let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3'];
 
 let numberofQuizQuestions=questions.length;
 let correctAnswer = 'Correct Answer: ';
