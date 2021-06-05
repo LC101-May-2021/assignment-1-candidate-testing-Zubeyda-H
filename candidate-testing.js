9a1db221bb61df84eb68c02c1751a363df638820
zubeyda <>
2021-06-05 05:21:12 +0000

    some change

--- candidate-testing.js
+++ candidate-testing.js
@@ -13,7 +13,7 @@ let questions = [
   'Who was the first American woman in space? ',
   'True or false: 5 kilometer == 5000 meters? ',
   '(5 + 3)/2 * 10 = ? ',
-  'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   'What is the minimum crew size for the ISS? '
 ];
 let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3'];
@@ -38,7 +38,7 @@ function askQuestion() {
    //candidateAnswer[i] = input.question(questions[i] + '\n' + yourAnswer).toLowerCase();
    candidateAnswer[i] = input.question(questions[i] + '\n' + yourAnswer);
    console.log("Correct Answer: " + correctAnswers[i]);
-     if (correctAnswers[i].toUpperCase() === candidateAnswer[i].toUpperCase()) {
+     if (correctAnswers[i].toLowerCase() === candidateAnswer[i].toLowerCase()) {
        numberofCorrectAnswers++;
      }
      console.log("\n");
