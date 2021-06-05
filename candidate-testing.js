0b344f4b116435ddcaaf113ebfd29a8f60a57c12
zubeyda <>
2021-06-05 04:20:28 +0000

    remove single quote

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,7 +15,7 @@ let questions = [
   'Who was the first American woman in space? ',
   'True or false: 5 kilometer == 5000 meters? ',
   '(5 + 3)/2 * 10 = ? ',
-  'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
+  'Given the array [8, Orbit, Trajectory, 45], what entry is at index 2? ',
   'What is the minimum crew size for the ISS? '
 ];
 let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3'];
@@ -46,7 +46,7 @@ function askQuestion() {
 
 }
 
-function gradeQuiz() {
+function gradeQuiz(numberofCorrectAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
   let grade=0;
 
@@ -68,7 +68,7 @@ function gradeQuiz() {
 function runProgram() {
   askForName();
   askQuestion();
-  gradeQuiz();  
+  gradeQuiz(numberofCorrectAnswers);  
 }
 
 //Part 2: Multiple Questions
