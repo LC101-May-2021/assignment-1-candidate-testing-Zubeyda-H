ddfe82696531cf7fe1679c7a69cb66185191fdc6
zubeyda <>
2021-06-05 15:59:26 +0000

    removed variable

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,50 +11,46 @@ let questions = [
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   'What is the minimum crew size for the ISS? '
 ];
-let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3'];
-let numberofQuizQuestions=questions.length;
+let correctAnswers = ['Sally Ride','true','40', 'Trajectory','3'];
 let correctAnswer = 'Sally Ride';
 let candidateAnswer='Your Answer: ';	
 let candidateAnswers = [];	
-let numberofCorrectAnswers = 0;
-//let result;
+
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
   candidateName= input.question("Your Name: ");
-  //console.log("Hello,",candidateName);
   console.log("\n");
 }
 
 function askQuestion() {
-  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  
-  for (let i = 0; i < questions.length; i++) {
-   //candidateAnswer[i] = input.question(questions[i] + '\n' + yourAnswer).toLowerCase();
+   for (let i = 0; i < questions.length; i++) {
    candidateAnswers[i] = input.question(questions[i] + '\n' + candidateAnswer);
    console.log("Correct Answer: " + correctAnswers[i]);
-     if (correctAnswers[i].toLowerCase() === candidateAnswers[i].toLowerCase()) {
-       numberofCorrectAnswers++;
-     }
-     console.log("\n");
+   console.log("\n");
  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  let grade = numberofCorrectAnswers / numberofQuizQuestions;
-  //let result = grade * 100;
-  if (numberofCorrectAnswers >= 4){
-    console.log(`>>> Overall Grade: ${grade * 100}% (${numberofCorrectAnswers} of ${numberofQuizQuestions} responses correct) <<<`);
+  let grade = 0;
+    for (let i = 0; i < questions.length; i++) {
+      if (correctAnswers[i].toLowerCase() === candidateAnswers[i].toLowerCase()) {
+       grade++;
+     }
+    }
+  
+  if (grade >= 4){
+    console.log(`>>> Overall Grade: ${grade/questions.length * 100}% (${grade} of ${questions.length} responses correct) <<<`);
     
     console.log(">>> Status: PASSED <<<");
 
    } else {
-    console.log(`>>> Overall Grade: ${grade * 100}% (${numberofCorrectAnswers} of ${numberofQuizQuestions} responses correct) <<<`);
+    console.log(`>>> Overall Grade: ${grade/questions.length * 100}% (${grade} of ${questions.length} responses correct) <<<`);
 
     console.log(" >>> Status: FAILED <<< ");
    }
+  grade = grade/questions.length * 100;
   return grade;
 }
 
@@ -65,7 +61,6 @@ function runProgram() {
 }
 
 //Part 2: Multiple Questions
-
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
@@ -75,8 +70,6 @@ module.exports = {
   candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
-  numberofCorrectAnswers: numberofCorrectAnswers,
-  numberofQuizQuestions: numberofQuizQuestions,
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
