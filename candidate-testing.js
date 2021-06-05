66dfe157843b1e63bde60f49b0f00c3ee9e778d2
zubeyda <>
2021-06-05 04:11:08 +0000

    update questions.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,9 +3,9 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName //
 let candidateName= "";
 // TODO 1.2a: Define question,correctAnswer, and candidateAnswer //
-let question= "Candidate Name:";
+let question= 'Who was the first American woman in space? ';
 //let correctAnswer="CorrectAnswer";
-let candidateAnswer="Your Answer";
+//let candidateAnswer;
 //let questions;
 //let correctAnswers;
 //let candidateAnswers;
@@ -13,21 +13,20 @@ let candidateAnswer="Your Answer";
 let numberofCorrectAnswers = 0;
 let questions = [
   'Who was the first American woman in space? ',
-  'True or false: 5000 meters = 5 kilometers. ',
+  'True or false: 5 kilometer == 5000 meters? ',
   '(5 + 3)/2 * 10 = ? ',
   'Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? ',
-  'What is the minimum crew size for the International Space Station (ISS)? '
+  'What is the minimum crew size for the ISS? '
 ];
 let correctAnswers = ['Sally Ride','true', '40', 'Trajectory', '3'];
-
 let numberofQuizQuestions=questions.length;
-let correctAnswer = 'Correct Answer: ';
+let correctAnswer = 'Sally Ride';
 let yourAnswer='Your Answer: ';	
-let candidateAnswers = [];	
+let candidateAnswer = [];	
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName= input.question(question);
+  candidateName= input.question("Your Name: ");
   console.log("Hello,",candidateName);
   console.log("\n");
 }
@@ -36,10 +35,10 @@ function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
   
   for (let i = 0; i < questions.length; i++) {
-   candidateAnswers[i] = input
+   candidateAnswer[i] = input
     .question(questions[i] + '\n'+ yourAnswer).toLowerCase();
     console.log(correctAnswer + correctAnswers[i]);
-     if (correctAnswers[i] === candidateAnswers[i]) {
+     if (correctAnswers[i] === candidateAnswer[i]) {
        numberofCorrectAnswers++;
      }
      console.log("\n");
@@ -49,10 +48,10 @@ function askQuestion() {
 
 function gradeQuiz() {
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  //let grade;
+  let grade=0;
 
   //return grade;
-  let grade = (numberofCorrectAnswers) / (numberofQuizQuestions) * 100;
+  grade = (numberofCorrectAnswers) / (numberofQuizQuestions) * 100;
   if (numberofCorrectAnswers >= 4){
     console.log(`>>> Overall Grade: ${grade}% (${numberofCorrectAnswers} of ${numberofQuizQuestions} responses correct) <<<`);
     
@@ -83,7 +82,7 @@ module.exports = {
   candidateAnswer: candidateAnswer,
   questions: questions,
   correctAnswers: correctAnswers,
-  candidateAnswers: candidateAnswers,
+  //candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 }
